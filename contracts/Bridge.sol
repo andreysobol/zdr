48b29f55948f8742bece83c5b7ec2114d019d62b
Andrey Sobol <andreysobol@protonmail.com>
2022-09-17 14:32:49 +0200

    rename

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -7,7 +7,7 @@ pragma solidity >=0.7.0 <0.9.0;
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
-contract Bridge {
+contract ZeroDataRollup {
 
     function deposit(
         address _l2Receiver,
